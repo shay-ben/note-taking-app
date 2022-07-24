eba8fac3bbd665ee71da727a4f729e78c49ac960
Your Name <you@example.com>
2022-07-24 18:39:44 -0400

    set up html route

---
+++ Develop/server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const path = require('path');
+const { clog } = require('./middleware/clog');
+const api = require('./routes/api-routes/index.js');
+
+const PORT = process.env.port || 3001;
+
+const app = express();
+
+
+// Middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/api', api);
+
+app.use(express.static('public'));
+
+// GET Route for homepage
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, './publi'))
+);
+
+// GET Route for notes page
+app.get('/feedback', (req, res) =>
+  res.sendFile(path.join(__dirname, './routes'))
+);
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
